36b5883d8d9f265f7d42e270193b39e08dd99d40
Ousikai <mtfallsVR@protonmail.com>
2022-05-16 23:33:15 -0400

    Basic Project Layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import { Portals } from './components'
+{/* <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}> */}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Portals/>
     </div>
   );
 }
